d0973cd5b24359ca02b950aa60ae2e033feac991
Stefan Johnson <sj@scrimpycat.io>
2018-07-12 22:49:21 +1000

    Update dep and silence ambiguous warnings

--- mix.exs
+++ mix.exs
@@ -9,8 +9,8 @@ defmodule SvgBobEx.Mixfile do
             elixir: "~> 1.3",
             build_embedded: Mix.env == :prod,
             start_permanent: Mix.env == :prod,
-            deps: deps,
-            package: package
+            deps: deps(),
+            package: package()
         ]
     end
 
